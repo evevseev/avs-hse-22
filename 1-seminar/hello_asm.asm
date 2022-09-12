290857a54373ebaf95c816d10ae9c389fca371d3
Efmprof <evseev-2003@ya.ru>
2022-09-12 18:12:27 +0000

    Hello, Assembler!

--- 1-seminar/hello_asm.asm
+++ 1-seminar/hello_asm.asm
@@ -1,18 +1,18 @@
-global _start
-
 section .data
 	helloText: db "Hello, World!", 10
 	helloTextLen equ $-helloText
-section .text	
+section .text
+	global _start ; точка входа
+	
 _start: 
-	mov	rax,1
-	mov	rdi,1
-	mov	rsi,helloText
-	mov	rdx,helloTextLen
-	syscall
+	mov rax,1 ; system write
+	mov rdi,1 ; пишем в stdout 
+	mov rsi,helloText ; сообщение
+	mov rdx,helloTextLen ; его длинна
+	syscall ; вызываем
 		
 
-	mov	rax,60
-	mov	rdi,0
-	syscall
+	mov rax,60 ; system exit
+	mov rdi,0 ; с кодом успеха 0
+	syscall ; вызываем
 	
